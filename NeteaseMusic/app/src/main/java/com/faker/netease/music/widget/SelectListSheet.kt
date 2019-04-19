845c8f375e34431115521c5a0aac21ce380b64df
moqi-Git <moqi.kaze@gmail.com>
2019-04-20 01:44:00 +0800

    【02】添加基础 UI 页面模板，学习 MaterialDesign。 需要补习一下文档，熟练使用协同布局套件，BottomSheet，Toolbar 等。 尝试自定义View的 ToolBar 和 BottomSheet。

---
+++ NeteaseMusic/app/src/main/java/com/faker/netease/music/widget/SelectListSheet.kt
@@ -0,0 +1,13 @@
+package com.faker.netease.music.widget
+
+import android.support.design.widget.BottomSheetDialogFragment
+
+/**
+ *
+ * copyright: ekwing.com
+ * created by reol at 2019/4/19
+ */
+class SelectListSheet: BottomSheetDialogFragment() {
+
+
+}
